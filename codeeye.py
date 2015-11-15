6104bfcb2bb4b31d3744e21a083b7ee1316968a2
wgliang <liangcszzu@163.com>
2015-11-15 16:02:48 +0800

    Init the project
    define the data json and readme

---
+++ codeeye.py
@@ -0,0 +1,24 @@
+import sys,os  
+
+#TODO:Identify file-type  
+def file_type():
+	pass
+
+#TODO:Count the lines
+def line_count():
+	pass
+
+#TODO:Identify file
+def identify(f_path):  
+	pass
+
+#TODO:Sort the code by lines
+def code_sort():
+	pass
+
+#Walk all files
+def walk_files():
+	pass
+	
+if(__name__=='__main__'):  
+    
